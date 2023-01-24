e8fd2bd15485bfbd53a4cdb68d84a155276e3447
Arzdez <infagot@gmail.com>
2023-01-24 17:24:39 +0400

    фурье фильтр

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,7 +1,8 @@
 
-from ._eeg_filter import EegFilter
+from ._eeg_filter import EegFilter,furier
 
 
 __all__ = [
     "EegFilter",
+    "furier",
 ]
--- filters/_eeg_filter.py
+++ filters/_eeg_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from statistics import mean
-
+from scipy.fft import fft, ifft, fftfreq
 
 class EegFilter:
     """
@@ -37,6 +38,7 @@ class EegFilter:
             self._Half -= 1
 
         self.EEG_data = EEG_data
+        
         self.trend = np.zeros(
             (np.shape(self.EEG_data)[0] - self.W + 1, np.shape(self.EEG_data)[1])
         )
@@ -44,6 +46,9 @@ class EegFilter:
             (np.shape(self.EEG_data)[0] - self.W + 1, np.shape(self.EEG_data)[1])
         )
         self.del_pick_data = None
+        self.clear_data = np.zeros(
+            (np.shape(self.EEG_data)[0] - self.W + 1, np.shape(self.EEG_data)[1])
+        )
 
     # Скользящее среднее
     def _moving_avg(self, x, w=1501):
@@ -58,6 +63,29 @@ class EegFilter:
         X_detred = np.subtract(x[w2:len(x)-w2],y)
 
         return X_detred
+    def _fourier(self, X, sample_rate, plot_spectrum  = False ):
+        time_step = 1/sample_rate
+        N = len(X)
+        #Фурье образ
+        yf = fft(X)
+        #Часты
+        xf = fftfreq(N, time_step)[:N//2]
+        
+        #Зануляем ненужные частоты
+        points_per_gZ = len(xf)/(sample_rate/2)
+        #Убираем 50+-1 герц 
+        target_GZ_1 = int(points_per_gZ  * 49)
+        target_GZ_2 = int(points_per_gZ  * 51)
+        target_GZ_3 = int(points_per_gZ  * 99)
+        yf[target_GZ_1:target_GZ_2] = 0
+        yf[target_GZ_3:] = 0
+    
+    #убираем всё от 99 герц
+        if plot_spectrum:
+            plt.plot(xf, 2./N * np.abs(yf[0:N//2]))
+            plt.show()
+    
+        return ifft(yf)
 
     # Расчтё скользящего среднего для указанного ряда
     def moving_avg(self) -> list:
@@ -73,7 +101,7 @@ class EegFilter:
 
     # Вычитание тренда
     def detrend(self) -> list:
-        # инициализируем значение тренда
+        #инициализируем значение тренда
         self.moving_avg()
         # Заполняем таймлайн
         self.detrend_data[:, 0] = self.EEG_data[
@@ -94,17 +122,24 @@ class EegFilter:
 
         for i in range(1, shape[1]):
             # Если пик в 100000 раз больше среднего - удаляем
-            means = 100000 * mean(self.del_pick_data[:, i])
+            #means = 100000 * mean(self.del_pick_data[:, i])
             #print(means)
 
             for j in range(shape[0] - 1):
-                if self.del_pick_data[j, i] > means:
-                    # print("удаляю пик")
+                if abs(self.del_pick_data[j, i]) > 1:
+                    print(f"удаляю пик{j}")
                     self.del_pick_data[j, i] = (
                         self.del_pick_data[j - 1, i] + self.del_pick_data[j + 1, i]
                     ) / 2
 
         return self.del_pick_data
+    
+    def filter_fft(self):
+        
+        for i in range(1, np.shape(self.EEG_data)[1]):
+            self.clear_data[:, i] = self._fourier(self.del_pick_data[:, i], 1000, True)
+            
+        
 
     def get_data(self) -> tuple:
         if self.del_pick_data == None:
@@ -113,3 +148,30 @@ class EegFilter:
 
     def all_processing(self) -> None:
         self.del_pick()
+
+
+
+def furier(X, sample_rate, plot_spectrum  = False ):
+    time_step = 1/sample_rate
+    N = len(X)
+    #Фурье образ
+    yf = fft(X)
+    #Часты
+    xf = fftfreq(N, time_step)[:N//2]
+    
+    #Зануляем ненужные частоты
+    points_per_gZ = len(xf)/(sample_rate/2)
+    #Убираем 50+-1 герц 
+    target_GZ_1 = int(points_per_gZ  * 49)
+    target_GZ_2 = int(points_per_gZ  * 51)
+    target_GZ_3 = int(points_per_gZ  * 99)
+    yf[target_GZ_1:target_GZ_2] = 0
+    yf[target_GZ_3:] = 0
+    
+    #убираем всё от 99 герц
+    if plot_spectrum:
+        plt.plot(xf, 2./N * np.abs(yf[0:N//2]))
+        plt.show()
+    
+    return ifft(yf)
+        
